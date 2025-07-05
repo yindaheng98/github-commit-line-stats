3d28ad2e66b5056ec2332ca0f0d001b2708270f0
yindaheng98 <yindaheng98@gmail.com>
2025-07-04 17:26:01 -0700

    vendor in linguist

--- ghcls/extensions/linguist.py
+++ ghcls/extensions/linguist.py
@@ -1,9 +1,10 @@
 import os
 import requests
 import yaml
+import re
 vendor_url = "https://github.com/github-linguist/linguist/raw/refs/heads/main/lib/linguist/vendor.yml"
 languages_url = "https://github.com/github-linguist/linguist/raw/refs/heads/main/lib/linguist/languages.yml"
-vendor = yaml.safe_load(requests.get(vendor_url).text)
+vendors = yaml.safe_load(requests.get(vendor_url).text)
 languages = yaml.safe_load(requests.get(languages_url).text)
 extension_dict = {}
 filename_dict = {}
@@ -12,11 +13,20 @@ for lang in languages:
         extension_dict[ext.lower()] = lang
     for filename in languages[lang].get("filenames", []):
         filename_dict[filename.lower()] = lang
+vendor_list = []
+for vendor in vendors:
+    vendor_list.append(re.compile(vendor, re.IGNORECASE))
 
 
 def get_language_by_filename(filename: str) -> str:
+    lang = "unknown"
     if os.path.basename(filename).lower() in filename_dict:
-        return filename_dict[os.path.basename(filename).lower()]
+        lang = filename_dict[os.path.basename(filename).lower()]
     if os.path.splitext(filename)[1].lower() in extension_dict:
-        return extension_dict[os.path.splitext(filename)[1].lower()]
-    return "unknown"
+        lang = extension_dict[os.path.splitext(filename)[1].lower()]
+    if lang == "unknown":
+        return "unknown"
+    for vendor in vendor_list:
+        if vendor.search(filename):
+            return "vendor"
+    return lang
